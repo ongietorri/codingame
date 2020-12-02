bab4b24aeb89c9d8ce1502f27339773b1a74648b
ongietorri <till@user-PC.localdomain>
2020-12-02 17:50:24 +0100

    stock exchange losses

---
+++ medium/Stock Exchange Losses/stock-exchange-losses.py
@@ -0,0 +1,16 @@
+# https://www.codingame.com/ide/puzzle/stock-exchange-losses
+
+hi, low, v_prev = 0, 2**31, None
+max_drawdown = 0
+n = int(input())
+for i in input().split():
+    v = int(i)
+    if v_prev is not None and v_prev > v:  # drawdown
+        hi = max(hi, v_prev)
+        low = min(low, v)
+        max_drawdown = min(low - hi, max_drawdown)
+    else:  # reset the low
+        low = 2**31
+    v_prev = v
+
+print(max_drawdown)
